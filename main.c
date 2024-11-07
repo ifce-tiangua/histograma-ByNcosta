c7133b24cc1ffb2078a1ad1c2685c4fddc337596
ByNcosta <nonatocosta212@gmail.com>
2024-11-07 19:42:15 -0300

    12

--- main.c
+++ main.c
@@ -2,9 +2,36 @@
 #include <stdlib.h>
 
 int main(){
-
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-    
+     int NrCateg;
+    printf( "INFORME A QUANTIDADE DE CATEGORIAS: ");
+    scanf( "%d", &NrCateg);
+    int* Nrvendas[NrCateg];
+        for (int i = 0; i < NrCateg; i++) {
+        Nrvendas[i] = (int*)malloc(sizeof(int));
+        if (Nrvendas[i] == NULL) {
+            printf( "Erro memoria alocada!.\n");
+             }
+        printf( "DIGITE O NUMERO DE VENDAS PARA CATEGORIA_%d: ", i + 1);
+        scanf( "%d",Nrvendas[i]);
+    }
+      ImprimirHist(Nrvendas, NrCateg);
+    for (int i = 0; i < NrCateg; i++){
+        free(Nrvendas[i]);
+    }
+    free(Nrvendas);
     return 0;
+}
+void ImprimirHist(int* Nrvendas[], int NrCateg){
+    for (int i = 0; i < NrCateg; i++) {
+        printf("%d ", *Nrvendas[i]);
+        if (*Nrvendas[i] > 0) {
+          for (int j = 0; j < *Nrvendas[i]; j++) {
+                printf("*");
+            }
+        } else {
+        printf("0");
+        }
+        printf("\n");
+    }
+
 }
