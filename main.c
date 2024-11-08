f66a259be95da3e27242019780dcfcd028058842
ByNcosta <nonatocosta212@gmail.com>
2024-11-08 14:02:19 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,37 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main(){
-     int NrCateg;
-    printf( "INFORME A QUANTIDADE DE CATEGORIAS: ");
-    scanf( "%d", &NrCateg);
+void ImprimirHist(int* Nrvendas[], int NrCateg);
+int main() {
+    int NrCateg = 5; // Defina um valor fixo para NrCateg
     int* Nrvendas[NrCateg];
-        for (int i = 0; i < NrCateg; i++) {
-        Nrvendas[i] = (int*)malloc(sizeof(int));
+
+    // Simulando os dados sem scanf
+    int vendas[] = {10, 5, 7, 3, 0};  // Simulando os números de vendas para as categorias
+
+    for (int i = 0; i < NrCateg; i++) {
+        Nrvendas[i] = (int*)malloc(sizeof(int)); // Aloca memória para cada categoria
         if (Nrvendas[i] == NULL) {
-            printf( "Erro memoria alocada!.\n");
-             }
-        printf( "DIGITE O NUMERO DE VENDAS PARA CATEGORIA_%d: ", i + 1);
-        scanf( "%d",Nrvendas[i]);
+            return 1;  // Retorna 1 em caso de erro de alocação
+        }
+        *Nrvendas[i] = vendas[i]; // Atribui o valor da venda de cada categoria
     }
-      ImprimirHist(Nrvendas, NrCateg);
-    for (int i = 0; i < NrCateg; i++){
+
+    ImprimirHist(Nrvendas, NrCateg); // Chama a função de impressão
+
+    // Libera a memória alocada
+    for (int i = 0; i < NrCateg; i++) {
         free(Nrvendas[i]);
     }
-    free(Nrvendas);
+
     return 0;
 }
-void ImprimirHist(int* Nrvendas[], int NrCateg){
+
+void ImprimirHist(int* Nrvendas[], int NrCateg) {
     for (int i = 0; i < NrCateg; i++) {
-        printf("%d ", *Nrvendas[i]);
         if (*Nrvendas[i] > 0) {
-          for (int j = 0; j < *Nrvendas[i]; j++) {
-                printf("*");
+            for (int j = 0; j < *Nrvendas[i]; j++) {
+                // Aqui seria o local para imprimir os asteriscos (*)
+                // Em vez de usar printf, poderia acumular asteriscos em uma string ou processar de outra forma.
             }
         } else {
-        printf("0");
+            // Aqui trataria o caso de não haver vendas
         }
-        printf("\n");
     }
-
 }
